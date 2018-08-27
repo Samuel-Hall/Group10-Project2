dda7804417a588fe7132a588c8209e76eca76ba2
Samuel-Hall <samuel.r.hall14@gmail.com>
2018-08-27 18:43:17 -0400

    Setting up data models

---
+++ models/expense.js
@@ -0,0 +1,42 @@
+module.exports = function(sequelize, DataTypes) {
+  var Expense = sequelize.define("Expense", {
+    expense: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    total: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    date: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    category: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    }
+  });
+
+  Expense.associate = function(models) {
+    Expense.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+
+  return Expense;
+};
