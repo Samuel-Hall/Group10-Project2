93e20fa17eabb8e265553b2736e0217bf24515bd
Samuel-Hall <samuel.r.hall14@gmail.com>
2018-08-30 19:11:35 -0400

    read from db

--- models/schema.sql
+++ models/schema.sql
@@ -7,6 +7,7 @@ USE expensedb;
 
 
 INSERT INTO expenses (expense, total, date, category) VALUES ("Test1", 14.05, '2018-08-28', "TestCategory");
+INSERT INTO expenses (expense, total, date, category) VALUES ("Test2", 19.05, '2018-08-29', "TestCategory");
 INSERT INTO users (userName, password, mobile) VALUES ("TestUser1", "PassworD", 1234567891);
 SELECT * FROM expenses;
 SELECT * FROM users;
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,23 +2,30 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Load index page
-  app.get("/", function(req, res) {
-    // db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        // examples: dbExamples
-      });
-    // });
-  });
+  // app.get("/", function(req, res) {
+  //   db.Expense.findAll({}).then(function(dbExpenses) {
+  //     res.render("index", {
+  //       msg: "Welcome!",
+  //       examples: dbExpenses
+  //     });
+  //   });
+  // });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
+  app.get("/history", function(req, res) {
+    res.render("history", {
+      msg: "Expense history"
     });
   });
+  // Load example page and pass in an example by id
+  // app.get("/example/:id", function(req, res) {
+  //   db.Expense.findOne({ where: { id: req.params.id } }).then(function(
+  //     dbExpenses
+  //   ) {
+  //     res.render("example", {
+  //       example: dbExpenses
+  //     });
+  //   });
+  // });
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
