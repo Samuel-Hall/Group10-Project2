43bb562372f2cb420a55225405244a4ea414fad2
Trina Raye <jonescatrina1@gmail.com>
2018-08-30 20:36:01 -0400

    updates to add

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,7 +10,8 @@ module.exports = function(app) {
 
   // Create a new example
   app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
+    console.log(req);
+    db.Expense.create(req.body).then(function(dbExample) {
       res.json(dbExample);
     });
   });
