349c87e0fc569b2f782600ccd8cf413e428b664a
unknown <ahlevitt@gmail.com>
2018-09-04 15:13:53 -0400

    Added new views for index (sign-in), signup (create account) and home (main view with option to add expenses). Updated and added to HTML routes.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -11,6 +11,26 @@ module.exports = function(app) {
     });
     // });
   });
+
+
+  app.get("/signup", function(req, res) {
+    // db.Example.findAll({}).then(function(dbExamples) {
+    res.render("signup", {
+      msg: "Register!"
+      // examples: dbExamples
+    });
+    // });
+  });
+
+  app.get("/home", function(req, res) {
+    // db.Example.findAll({}).then(function(dbExamples) {
+    res.render("home", {
+      msg: "Homepage!"
+      // examples: dbExamples
+    });
+    // });
+  });
+
   // Load example page and pass in an example by id
   // app.get("/example/:id", function(req, res) {
   //   db.Expense.findOne({ where: { id: req.params.id } }).then(function(
